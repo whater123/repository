56ca8d7f917172b936307178d14ba1a01f6477dd
obuivy <310714289@qq.com>
2020-07-31 17:39:28 +0800

    Merge branch 'dev' of github.com:whater123/repository into dev
    # Conflicts:
    #	RecruitmentWebsite/.idea/workspace.xml
    #	RecruitmentWebsite/target/classes/com/springboot/mybatis/MybatisApplication.class
    #	RecruitmentWebsite/target/classes/com/springboot/mybatis/controller/FreshmanSignUp.class
    #	RecruitmentWebsite/target/classes/com/springboot/mybatis/controller/LoginAndRegister.class
    #	RecruitmentWebsite/target/classes/com/springboot/mybatis/controller/ManagerController.class
    #	RecruitmentWebsite/target/classes/com/springboot/mybatis/controller/PageJumpController.class
    #	RecruitmentWebsite/target/classes/com/springboot/mybatis/dao/ManagerMapper.class
    #	RecruitmentWebsite/target/classes/com/springboot/mybatis/dao/UerRegisterMapper.class
    #	RecruitmentWebsite/target/classes/com/springboot/mybatis/pojo/LoginUser.class
    #	RecruitmentWebsite/target/classes/com/springboot/mybatis/service/Imp/LoginAndRegisterImp.class
    #	RecruitmentWebsite/target/classes/com/springboot/mybatis/service/Imp/ManagerImp.class
    #	RecruitmentWebsite/target/classes/com/springboot/mybatis/service/LoginAndRegisterService.class
    #	RecruitmentWebsite/target/classes/com/springboot/mybatis/service/ManagerService.class
    #	RecruitmentWebsite/target/classes/com/springboot/mybatis/util/SystemParam.class
    #	RecruitmentWebsite/target/test-classes/com/springboot/mybatis/MybatisApplicationTests.class

--- RecruitmentWebsite/target/classes/com/springboot/mybatis/dao/NoticeMapper.java
+++ RecruitmentWebsite/target/classes/com/springboot/mybatis/dao/NoticeMapper.java
@@ -2,10 +2,12 @@ package com.springboot.mybatis.dao;
 
 import com.springboot.mybatis.pojo.Notice;
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Mapper
+@Repository
 public interface NoticeMapper {
 
     Notice getNoticeById(Integer id);
--- RecruitmentWebsite/target/classes/com/springboot/mybatis/service/Imp/FreshmanSignUpImp.java
+++ RecruitmentWebsite/target/classes/com/springboot/mybatis/service/Imp/FreshmanSignUpImp.java
@@ -12,31 +12,86 @@ import java.util.Random;
 
 public interface FreshmanSignUpImp {
 
+    //判断类
+    /**
+    * 判断user中的qq号是否已经在数据库中存在
+    * */
     boolean isQqExisted(User user);
 
+    /**
+     * 判断user中的id是否已经在数据库中存在
+     * */
     boolean isIdExisted(User user);
 
-    void insertSignUp(User user);
-
-    void insertSignUp_mogul(User user);
-
-    void updateLoginId(String num, String id);
-
+    /**
+     * 判断通过学号（number）是否报名
+     * */
     boolean isSignedUp(String number);
 
-    User getLoginUser(HttpServletRequest request) throws JsonProcessingException;
-
-    User getContextUser(String context) throws JsonProcessingException;
-
-    User mergeUser(User loginUser,User contextUser) throws JsonProcessingException;
-
-    User getUser_normal(String context,HttpServletRequest request) throws JsonProcessingException;
-
-    User getUser_mogul(String context,HttpServletRequest request) throws JsonProcessingException;
-
-    void setRandomId(User user);
-
+    /**
+     * 判断该User对象中的报名数据是否合法
+     * */
     StateCode isUserRule(User user);
 
+
+    //数据库操作
+    /**
+     * 在user_signup中插入user的部分数据
+     * */
+    void insertSignUp(User user);
+
+    /**
+     * 在user_signup插入user的部分数据
+     * 在user_dalao插入user的部分数据
+     * */
+    void insertSignUp_mogul(User user);
+
+    /**
+     * 在user_register依据num修改id
+     * */
+    void updateLoginId(String num, String id);
+
+    /**
+     * 通过学号获得
+     * user_register和user_signup数据的User对象
+     * */
+    User getUserByNumber(String number);
+
+
+    //User对象操作
+    /**
+     * 获得报名后全数据的User对象（普通报名）
+     * */
+    User getUser_normal(String context,HttpServletRequest request) throws JsonProcessingException;
+
+    /**
+     * 获得报名后全数据的User对象（大佬报名）
+     * */
+    User getUser_mogul(String context,HttpServletRequest request) throws JsonProcessingException;
+
+    /**
+     * 通过session获得当前登录User对象
+     * */
+    User getLoginUser(HttpServletRequest request) throws JsonProcessingException;
+
+    /**
+     * 通过json转化成User对象
+     * */
+    User getContextUser(String context) throws JsonProcessingException;
+
+    /**
+     * 在User对象里设置随机id
+     * */
+    void setRandomId(User user);
+
+    /**
+     * 删除User对象中的报名数据中的空格（除开特长和个人说明）
+     * */
     User deleteUserBlank(User user);
+
+    /**
+     * 合并 当前登录的User对象和报名生成json所转化的User对象
+     * */
+    User mergeUser(User loginUser,User contextUser) throws JsonProcessingException;
+
 }
